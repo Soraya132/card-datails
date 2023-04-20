110c0809fbf0147f1a80e035d8e8fc4583297b83
soraya <sorayanoor132@gmail.com>
2023-04-20 05:16:47 -0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,24 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens:{
+
+      "sm":"400px",
+      "md":"600px",
+      "lg":"1000px"
+      
+    },
+    colors:{
+      "Lineargradient(activeinputborder)": "hsl(249, 99%, 64%)",
+      "border":"hsl(278, 94%, 30%)",
+      "Red(inputerrors)":" hsl(0, 100%, 66%)",
+      "Lightgrayishviolet": "hsl(270, 3%, 87%)",
+      "Darkgrayishviolet": "hsl(279, 6%, 55%)",
+      "Verydarkviolet": "hsl(278, 68%, 11%)",
+      "White": "hsl(0, 0%, 100%)"
+          },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+     
     },
   },
   plugins: [],
